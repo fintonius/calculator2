8df5cdc224c6013b28955f6740d2a42b2c97564b
Fintan Ryan <fintonius.mcginty@gmail.com>
2022-09-23 11:40:09 +0100

    take user input via keyboard-numbers and/or button-clicks of onscreen calculator

--- script.js
+++ script.js
@@ -16,3 +16,21 @@
 
 const numberDisplay = document.querySelector('.number-display');
 numberDisplay.textContent = 'DISPLAY';
+
+//alternative to the Wes Bos drum-kit approach, which I still quite like :) :
+//  listen for keydown, if it's keyCode/id(?) matches regexListA then call A-function,
+//  if it matches regexListB call B-function, otherwise return.
+const numbers = /[1234567890]/;
+function numPress(e) {
+	if (numbers.test(e.key)) {
+		console.log('a');
+	} else if (numbers.test(e.target.id)) {
+		console.log('success!');
+	} else {
+		return
+	};
+};
+window.addEventListener('keydown', numPress);
+document.querySelectorAll('.number-button').forEach(btn => {
+	btn.addEventListener('click', numPress)
+});
