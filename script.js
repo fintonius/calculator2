e596741108accea0ae9a5973c46960c2bcac0039
Fintan Ryan <fintonius.mcginty@gmail.com>
2022-09-23 17:59:35 +0100

    check to see what sort of event called function - click or keydown

--- script.js
+++ script.js
@@ -15,22 +15,45 @@
 // step 1: allow user to input numbers through buttons & keyboard
 
 const numberDisplay = document.querySelector('.number-display');
-numberDisplay.textContent = 'DISPLAY';
+numberDisplay.textContent = '';
 
 //alternative to the Wes Bos drum-kit approach, which I still quite like :) :
 //  listen for keydown, if it's keyCode/id(?) matches regexListA then call A-function,
 //  if it matches regexListB call B-function, otherwise return.
-const numbers = /[1234567890]/;
-function numPress(e) {
-	if (numbers.test(e.key)) {
-		console.log('a');
-	} else if (numbers.test(e.target.id)) {
-		console.log('success!');
-	} else {
-		return
-	};
-};
+//NEED TO REMEMBER TO ONLY CHECK/PASS '.' ONCE PER NUMBER!
+const numbers = /[1234567890.]/;
+const operators = /[-+=\/]/
+
+//takes user number input via keyboard & calculator button-clicks & passes to calculatorDisplay
 window.addEventListener('keydown', numPress);
-document.querySelectorAll('.number-button').forEach(btn => {
+document.querySelectorAll('.number-button, .operator-button').forEach(btn => {
 	btn.addEventListener('click', numPress)
 });
+
+//can I test for numbers || operators but ALSO pass either btn//click .ids to display || call operator?!?
+//should keyboard events call one function and click events call another?!
+//OR is it one event for numbers and one event for operators?
+//OR OR does the one function first check if it was operator or number then run relevant sub-function?!?!?!?!?
+function numPress(e) {
+	//check to see what sort of event called function - click or keydown
+	
+	if (e.type == 'click') {
+		operators.test(e.target.id) ? console.log('a') : numberDisplay.textContent += e.target.id;
+	} else {
+		numbers.test(e.key) ? numberDisplay.textContent += e.key : console.log('b');
+	}
+	// if (numbers.test(e.key)) {
+	// 	numberDisplay.textContent += e.key;
+	// } else if (operators.test(e.target.id)) {
+	// 	console.log('woohoo!');
+	// } 
+	// else if (numbers.test(e.target.id)) {
+	// 	numberDisplay.textContent += e.target.id;
+	// } else {
+		return
+	};
+// };
+
+//pass
+
+//takes user operator input via keyboard and clicking calculator buttons
