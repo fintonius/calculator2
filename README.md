f4fa340a086e14237edc92e315adced5db18f45d
Fintan Ryan <fintonius.mcginty@gmail.com>
2022-10-05 11:59:32 +0100

    implement step 4 & step 5 changes

--- README.md
+++ README.md
@@ -10,3 +10,33 @@ I also want to try to use different methods from the first calculator for comple
 the project. I stuck very much with what I knew for the first one so this will push
 me to learn and understand more different approaches to coding. Don't want to get
 stuck/tunnel visioned on just the one way of doing things!
+
+//Pseudo-code & notes:
+// - user inputs number by either/and clicking onscreen buttons and/or keyboard
+//  - the number is shown on the calculator display
+// - user presses/clicks an operator button
+//  - this passes the number to the code to store
+//  - the operator is passed to the code to store
+//  - the calculator display is cleared
+// - user inputs another number
+//  - the number is shown on the calculator display
+// - user presses/clicks equals operator
+//  - this passes the number to the code to store
+//  - a function is called to solve the relevant equation
+//  - the answer to equation displayed
+//
+// step 1: allow user to input numbers through buttons & keyboard
+// step 2: pressing operator button: 
+//	- passes number to object||variable?
+//	- passes operatorID to object||variable?
+//	- clears numberDisplay
+// step 3: pressing equals button:
+//	- passes number to object||variable?
+//	- calls function to solve equation
+//	- passes answer to numberDisplay
+// step 4: clear display of equation answer when new number submitted by user
+
+
+//I feel like there's something I'm missing here... the problem for me is how to reconcile
+//being able to access properties of either universally rather than needing
+//e.target.id for clickEvents and e.id for keyEvents...
--- script.js
+++ script.js
@@ -1,47 +1,84 @@
-//Pseudo-code & notes:
-// - user inputs number by either/and clicking onscreen buttons and/or keyboard
-//  - the number is shown on the calculator display
-// - user presses/clicks an operator button
-//  - this passes the number to the code to store
-//  - the operator is passed to the code to store
-//  - the calculator display is cleared
-// - user inputs another number
-//  - the number is shown on the calculator display
-// - user presses/clicks equals operator
-//  - this passes the number to the code to store
-//  - a function is called to solve the relevant equation
-//  - the answer to equation displayed
-//
-// step 1: allow user to input numbers through buttons & keyboard
-// step 2: pressing operator button: 
-//	- passes number to object||variable?
-//	- passes operatorID to object||variable?
-//	- clears numberDisplay
-// step 3: pressing equals button:
-//	- passes number to object||variable?
-//	- calls function to solve equation
-//	- passes answer to numberDisplay
-// step 4: clear display of equation answer when new number submitted by user
+// step 5: fix following issues:
+//	- stop numberDisplay changing size when text entered - DONE
+//	- add drop shadow to text - DONE
+//	- change font to calculator font - DONE
+//	- decimal point can only be added once
+// 	- multiple operators can be added to equation
+//	- when equals is pressed the answer can be used in next equation
+//	- add character limit to numberDisplay - BEST WAY SEEMS TO BE USING <input>
 
 let numberDisplay = document.getElementById('display');
 function clearDisplay() {
-	numberDisplay.value = '';
+	numberDisplay.textContent = '';
 };
-//NEED TO REMEMBER TO ONLY CHECK/PASS '.' ONCE PER NUMBER!
-const numbers = /[1234567890.]/;
-const operators = /[*-+=\/]/
-let equalsClicked;
 
-//I feel like there's something I'm missing here... the problem for me is how to reconcile
-//being able to access properties of either universally rather than needing
-//e.target.id for clickEvents and e.id for keyEvents...
-window.addEventListener('keydown', numPress);
+const numbers = /[0-9\.?]/;
+const operators = /[Enter*\-+=\/]/
+let equalsClicked = false; //this feels like a janky way around the equals problem!
 
-document.querySelectorAll('.number-button, .operator-button').forEach(btn => {
-	btn.addEventListener('click', numClick)
+//handling keyboard input
+window.addEventListener('keydown', (e) => 	{
+	if ((!numbers.test(e.key)) && (!operators.test(e.key))) {
+		return // ignores unwanted keypresses
+	} else {
+		numbers.test(e.key) ? numPress(e): operatorPress(e);
+	}
 });
 
+//use a loop to check each keypress before adding it to numberDisplay?
+function numPress(e) { //have a function for equalsClicked & one for number?
+ if (equalsClicked) {
+		numberDisplay.textContent = '';
+		numberDisplay.textContent += e.key;
+		equalsClicked = false;
+	} else {
+			numberDisplay.textContent += e.key;
+	}
+};
 
+function operatorPress(e) {
+	if ((e.key == '=') || (e.key == 'Enter')) {
+		equation.num2 = numberDisplay.textContent;
+		numberDisplay.textContent = equation.solution();
+		equalsClicked = true;
+		clearEquation();
+	} else {
+			equation.num1 = numberDisplay.textContent;
+			equation.operator = e.key;
+			clearDisplay();
+	}
+};
+
+//handling on-screen button clicks
+document.querySelectorAll('.number-button').forEach(btn => {
+	btn.addEventListener('click', numClick)
+});
+document.querySelectorAll('.operator-button').forEach(btn => {
+	btn.addEventListener('click', operatorClick)
+});
+
+function numClick(e) { //can this be a loop?
+	if (equalsClicked) {
+			numberDisplay.textContent = '';
+			numberDisplay.textContent += e.target.id;
+			equalsClicked = false;
+		} else {
+				numberDisplay.textContent += e.target.id;
+		}
+};
+
+function operatorClick(e) {
+	equation.num1 = numberDisplay.textContent;
+	equation.operator = e.target.id;			
+	clearDisplay();
+};
+
+function equalClick() {
+	equation.num2 = numberDisplay.textContent;
+	numberDisplay.textContent = equation.solution();
+	equalsClicked = true;
+	clearEquation();
+}
 
 //stores the numbers & operator as they are entered then calculates answer when '=' is clicked/pressed
 const equation = {
@@ -51,7 +88,7 @@ const equation = {
   solution : function() {
 		switch(this.operator) {
 			case '+':
-				return this.num1 + this.num2;
+				return +this.num1 + +this.num2;
 				break;
 			case '-':
 				return this.num1 - this.num2;
@@ -73,90 +110,3 @@ function clearEquation() {
 	equation.num2 = 0;
 	equation.operator = '';
 }
-
-function numClick(e) { //handles click events
-	if (e.target.id == '=') {
-		equation.num2 = numberDisplay.value;
-		numberDisplay.value = equation.solution();
-		equalsClicked = true;
-		clearEquation();
-	} else if (numbers.test(e.target.id)) { 
-			if(equalsClicked) {
-				numberDisplay.value = '';
-				numberDisplay.value += e.target.id;
-				equalsClicked = false;
-			} else {
-				numberDisplay.value += e.target.id;
-			}
-	} else {
-			equation.num1 = numberDisplay.value;
-			equation.operator = e.target.id;			
-			clearDisplay();
-		};
-};
-
-function numPress(e) {
-	if ((!numbers.test(e.key)) && (!operators.test(e.key))) {
-		return // ignores unwanted keypresses
-	} else {
-		numbers.test(e.key) ? numberDisplay.value += e.key : operatorPress(e);
-	}
-}
-
-function operatorPress(e) {
-				equation.num1 = +numberDisplay.value;
-				equation.operator = e.key;
-				clearDisplay();
-}
-
-// function numPress(e) { //handles keydown events
-// 	if ((e.key == '=') || (e.key == 'Enter')) {
-// 		equation.num2 = +numberDisplay.textContent;
-// 		numberDisplay.textContent  = equation.solution();
-// 		equalsClicked = true;
-// 		clearEquation();
-// 	} else if (numbers.test(e.key)) { 
-// 		if(equalsClicked) {
-// 			numberDisplay.textContent = '';
-// 			numberDisplay.textContent += e.key;
-// 			equalsClicked = false;
-// 		} else {
-// 			numberDisplay.textContent += e.key;
-// 		}
-// 	} else if (operators.test(e.key)) {
-// 			equation.num1 = +numberDisplay.textContent;
-// 			equation.operator = e.key;
-// 			clearDisplay();
-// 	} else {
-// 			return //makes sure unwanted keypresses are being ignored 
-// 	};
-// }
-
-//OR, ONE FUNCTION TO RULE THEM ALL! this was getting too messy and also basically was the same
-//code repeated so it made little sense to try mash it all into 1 function when 2 was cleaner
-// function numPress(e) {
-// 	if (e.type == 'click') { 	//check to see what sort of event called function - click or keydown
-// 			if (e.target.id == '=') {
-// 				console.log('test')
-// 			}
-// 			 else if (numbers.test(e.target.id)) { //handles click events
-// 				numberDisplay.textContent += e.target.id;
-// 			 } else {
-// 				equation['num1'] = +numberDisplay.textContent;
-// 				equation['operator'] = e.target.id;
-// 				clearDisplay();
-// 			}
-// 		} else { 
-// 			if (numbers.test(e.key)) { //handles keydown events
-// 				numberDisplay.textContent += e.key;
-// 			 } else if (operators.test(e.key)) {
-// 				equation['num1'] = +numberDisplay.textContent;
-// 				equation['operator'] = e.key;
-// 				clearDisplay();
-// 			 } else {
-// 				return //makes sure unwanted keypresses are being ignored 
-// 			 };
-// 		}; 
-// 	};
-
-//only alternative I can see is 2 functions: numPress() & numClick()
