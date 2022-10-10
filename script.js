7a4f3c84cf9ba7f2a7bf1be7e7eecab66a5f8699
Fintan Ryan <fintonius.mcginty@gmail.com>
2022-10-10 09:46:43 +0100

    add max char. length to numberDisplay, allow '.' only once & update button font

--- script.js
+++ script.js
@@ -2,12 +2,17 @@
 //	- stop numberDisplay changing size when text entered - DONE
 //	- add drop shadow to text - DONE
 //	- change font to calculator font - DONE
-//	- decimal point can only be added once
+//	- decimal point can only be added once - DONE!!!
+//	- fix text bottom being cut off - DONE
 // 	- multiple operators can be added to equation
 //	- when equals is pressed the answer can be used in next equation
-//	- add character limit to numberDisplay - BEST WAY SEEMS TO BE USING <input>
+//	- add character limit to numberDisplay - BEST WAY SEEMS TO BE USING <input> - DONE (sort of)
+//	- look at bug where certain combos of key'=' and clickInputs don't work
 
 //	step 6: CLEAN UP README FILE!!!
+
+//AS YOU TRY TO SOLVE EACH PROBLEM USE IT AS AN OPPORTUNITY TO IMPROVE THE OVERALL CODE//
+
 let number = [];
 const numbers = /[0-9\.?]/;
 const operators = /[Enter*\-+=\/]/
@@ -15,33 +20,38 @@ let equalsClicked = false; //this feels like a janky way around the equals probl
 
 let numberDisplay = document.getElementById('display');
 function clearDisplay() {
-	numberDisplay.textContent = '';
-	number = [];
+	numberDisplay.value = '';
+	number.length = 0;
 };
 
 //handling keyboard input
 window.addEventListener('keydown', (e) => 	{
 	if ((!numbers.test(e.key)) && (!operators.test(e.key))) {
-		return // ignores unwanted keypresses
+		console.log('hell world'); // ignores unwanted keypresses
 	} else {
 		numbers.test(e.key) ? addNumber(e.key): operatorPress(e);
-	}
-	;
+	};
 });
 
 function addNumber(e) {
 	((e == '.') && (number.includes('.'))) ? console.log('hell world') : number.push(e);//only allows '.' once
-	numberDisplay.textContent = number.join(''); 
+	
+	number.length = Math.min(number.length, 9);//limits the amount of digits in array/numberDisplay to 9.
+	//it's a bit artificial though, calculations on large numbers still overspill as the answer is displayed
+	//directly to numberDisplay rather than "through" the number array.
+	//MAYBE combine this with using <input type="text||number"> and adding a max length to that too?
+	//THEN how to handle large equations?
+	numberDisplay.value = number.join(''); 
 };
 
 function operatorPress(e) {
 	if ((e.key == '=') || (e.key == 'Enter')) {
-		equation.num2 = numberDisplay.textContent;
-		numberDisplay.textContent = equation.solution();
+		equation.num2 = numberDisplay.value;
+		numberDisplay.value = equation.solution();
 		equalsClicked = true;
 		clearEquation();
 	} else {
-			equation.num1 = numberDisplay.textContent;
+			equation.num1 = numberDisplay.value;
 			equation.operator = e.key;
 			clearDisplay();
 	}
@@ -57,24 +67,24 @@ document.querySelectorAll('.operator-button').forEach(btn => {
 
 function numClick(e) { //can this be a loop?
 	if (equalsClicked) {
-			numberDisplay.textContent = '';
-			numberDisplay.textContent += e.target.id;
+			numberDisplay.value = '';
+			numberDisplay.value += e.target.id;
 			equalsClicked = false;
 		} else {
 			((e.target.id == '.') && (number.includes('.'))) ? console.log('hell world') : number.push(e.target.id);//only allows '.' once
-			numberDisplay.textContent = number.join(''); 
+			numberDisplay.value = number.join(''); 
 		}
 };
 
 function operatorClick(e) {
-	equation.num1 = numberDisplay.textContent;
+	equation.num1 = numberDisplay.value;
 	equation.operator = e.target.id;			
 	clearDisplay();
 };
 
 function equalClick() {
-	equation.num2 = numberDisplay.textContent;
-	numberDisplay.textContent = equation.solution();
+	equation.num2 = numberDisplay.value;
+	numberDisplay.value = equation.solution();
 	equalsClicked = true;
 	clearEquation();
 }
@@ -108,6 +118,6 @@ function clearEquation() {
 	equation.num1 = 0;
 	equation.num2 = 0;
 	equation.operator = '';
-	number = [];
+	number.length = 0;
 }
 
